673c4902f98ac3428a934e483f8abd752b8ae6e7
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-19 19:48:19 +1030

    change from individual routes to resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-  post "/articles", to: "articles#create"
-  get "/articles/:id", to: "articles#show"
-  delete "/articles/:id", to: "articles#destroy"
-  put "/articles/:id", to: "articles#update"
-  patch "/articles/:id", to: "articles#update"
+  resources :articles, only: [:index, :create, :show, :destroy, :update]
+  # get "/articles", to: "articles#index"
+  # post "/articles", to: "articles#create"
+  # get "/articles/:id", to: "articles#show"
+  # delete "/articles/:id", to: "articles#destroy"
+  # put "/articles/:id", to: "articles#update"
+  # patch "/articles/:id", to: "articles#update"
 
   root to: "articles#index"
 end
