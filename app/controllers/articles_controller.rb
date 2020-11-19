47dc7fb76caabd87dad58ab6dd307d7aaf890854
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-19 15:15:40 +1030

    finish article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,20 +17,18 @@ class ArticlesController < ApplicationController
     # new won't trigger model validation
     # create will trigger model validation
     @article = Article.create(article_params)
-    render json: @article.errors
-
     if @article.errors.empty?
       render json: @article, status: :created
     else
-      render json: @article.errors, status: : unprocessable_entity
+      render json: @article.errors, status: :unprocessable_entity
     end 
   end
 
   def update
     # update will trigger model validation
-    @article = Article.update(article_params)
-    if @article.save
-      render json: @article, status: :created
+    @article.update(article_params)
+    if @article.errors.empty?
+      render json: @article, status: :ok
     else
       render json: @article.errors, status: :unprocessable_entity
     end
