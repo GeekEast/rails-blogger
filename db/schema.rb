4facc84dc0cf18941182db57582cf079a60a981d
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-20 12:13:21 +1030

    change users id as uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_001934) do
+ActiveRecord::Schema.define(version: 2020_11_20_014040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_001934) do
     t.index ["deleted_at"], name: "index_articles_on_deleted_at"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
