ef729951c9c7ac59ab6003c680809abd59c9fa25
ZenGeek <xiang.tan.p2109@gmail.com>
2020-11-20 20:58:36 +1030

    remove RemoveIndexOnUserUidAndProvider index

---
+++ db/migrate/20201120102752_remove_index_on_user_uid_and_provider.rb
@@ -0,0 +1,5 @@
+class RemoveIndexOnUserUidAndProvider < ActiveRecord::Migration[5.2]
+  def change
+    remove_index :users, name: :index_users_on_uid_and_provider
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_092846) do
+ActiveRecord::Schema.define(version: 2020_11_20_102752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2020_11_20_092846) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
